364eaa788d891ddb185c0e52b0753eefa5eef97f
zen-testing <1914301892@qq.com>
2024-08-16 23:24:17 +0800

    拆分函数

--- file/file.go
+++ file/file.go
@@ -9,6 +9,9 @@ import (
 	"strings"
 )
 
+/*
+使用find获取目录下全部文件
+*/
 func GetAllFile(root string) *[]string {
 	//root := "/mnt/e/video"
 	cmd := exec.Command("find", root, "-type", "f")
@@ -22,25 +25,57 @@ func GetAllFile(root string) *[]string {
 	return &paths
 }
 
-func GetVideoFile(s *[]string) {
+/*
+通过文件头判断是否为视频文件
+*/
+func GetVideoFile(s *[]string) []string {
+	var nonSatisfyingVideos []string
 	for _, path := range *s {
 		if path != "" {
 			if IsVideo(path) {
-				//fmt.Printf("单独文件%v\n", path)
-				p := FastMediaInfo.GetStandMediaInfo(path)
 				//println(p.Video.CodecID) //hvc1
 				//println(p.Video.Format)  //HEVC
-				if p.Video.Format == "HEVC" {
-					//fmt.Printf("满足条件的视频: %v\n", path)
-					satisfyingVideos = append(satisfyingVideos, path)
+				if GetNotH265ByMediainfo(path) && GetNotH265ByFfprob(path) {
+					fmt.Printf("跳过HEVC的视频: %v\n", path)
+					//satisfyingVideos = append(satisfyingVideos, path)
 				} else {
-					//fmt.Printf("不满足条件的视频: %v\n", path)
+					//fmt.Printf("不是HEVC的视频: %v\n", path)
 					nonSatisfyingVideos = append(nonSatisfyingVideos, path)
 				}
 			}
 		}
 	}
+	return nonSatisfyingVideos
 }
+
+/*
+通过mediainfo判断是否为非hevc视频
+非h265返回真
+*/
+func GetNotH265ByMediainfo(path string) bool {
+	p := FastMediaInfo.GetStandMediaInfo(path)
+	if p.Video.Format == "HEVC" {
+		//fmt.Printf("HEVC的视频: %v\n", path)
+		return false
+	} else {
+		//fmt.Printf("不是HEVC的视频: %v\n", path)
+		return true
+	}
+}
+
+/*
+通过ffprob再次判断是否为非hevc视频
+非h265返回真
+*/
+func GetNotH265ByFfprob(path string) bool {
+	cmd := exec.Command("ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=codec_name", "-of", "default=noprint_wrappers=1:nokey=1", path)
+	if output, _ := cmd.CombinedOutput(); string(output) == "hevc" {
+		return false
+	} else {
+		return true
+	}
+}
+
 func IsVideo(fp string) bool {
 	file, _ := os.Open(fp)
 	// We only have to pass the file header = first 261 bytes
@@ -51,17 +86,4 @@ func IsVideo(fp string) bool {
 		return true
 	}
 	return false
-}
-
-/*
-二次判断视频编码
-h265返回真
-*/
-func ffprob(s string) bool {
-	cmd := exec.Command("ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=codec_name", "-of", "default=noprint_wrappers=1:nokey=1", s)
-	if output, _ := cmd.CombinedOutput(); string(output) == "hevc" {
-		return true
-	} else {
-		return false
-	}
 }
