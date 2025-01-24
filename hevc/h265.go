132c459fbb6119f0a3e08a0ef238995357052cf6
zen <hasee>
2025-01-24 12:52:15 +0800

    统一使用h265软编码

--- hevc/h265.go
+++ hevc/h265.go
@@ -2,7 +2,6 @@ package hevc
 
 import (
 	"AnyVideosToH265/util"
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -19,21 +18,15 @@ func ProcessVideo2H265(fp string) {
 	}
 	mi := FastMediaInfo.GetStandMediaInfo(fp)
 	FrameCount := mi.Video.FrameCount
-	// if mi.Video.CodecID == "hvc1" || mi.Video.CodecID == "vp09" {
-	// 	log.Println("跳过已经转码的视频")
-	// 	return
-	// }
+	if mi.Video.CodecID == "hvc1" || mi.Video.CodecID == "vp09" {
+		log.Println("跳过已经转码的视频")
+		return
+	}
 	mp4 := strings.Replace(fp, filepath.Ext(fp), "_hevc.mp4", 1)
 	//"-strict" ,"-2" ,"-vf" ,"scale=-1:1080",
-	cmd := exec.Command("ffmpeg", "-i", fp, "-strict", "-2", "-vf", "scale=-1:1080", "-c:v", "libx265", "-tag:v", "hvc1",
-		"-ac", "1", "-map_chapters", "-1", mp4)
+	cmd := exec.Command("ffmpeg", "-i", fp, "-vf", "scale=if(gt(iw\\,ih)\\,1920\\,-2):if(gt(iw\\,ih)\\,-2\\,1080)", "-c:v", "libx265", "-tag:v", "hvc1", "-c:a", "libmp3lame", "-map_chapters", "-1", mp4)
 	if runtime.GOOS == "linux" && runtime.GOARCH == "arm64" {
-		cmd = exec.Command("ffmpeg", "-i", fp, "-strict", "-2", "-vf", "scale=-1:1080", "-threads", "1", "-c:v", "libx265", "-tag:v", "hvc1", "-c:a", "libopus", "-ac", "1", "-map_chapters", "-1", "-threads", "1", mp4)
-	}
-	if hostname, _ := os.Hostname(); hostname == "DESKTOP-VGFTVD8" {
-		fmt.Println("是神舟战神,可以使用cuda加速")
-		cmd = exec.Command("ffmpeg", "-i", fp, "-strict", "-2", "-vf", "scale=-1:1080", "-c:v", "h264_nvenc", "-tag:v", "hvc1",
-			"-ac", "1", "-map_chapters", "-1", mp4)
+		cmd = exec.Command("ffmpeg", "-i", fp, "-threads", "1", "-vf", "scale=if(gt(iw\\,ih)\\,1920\\,-2):if(gt(iw\\,ih)\\,-2\\,1080)", "-c:v", "libx265", "-tag:v", "hvc1", "-c:a", "libmp3lame", "-map_chapters", "-1", "-threads", "1", mp4)
 	}
 	log.Printf("生成的命令:%v\n", cmd.String())
 	if err := util.ExecCommand(cmd, FrameCount); err != nil {
